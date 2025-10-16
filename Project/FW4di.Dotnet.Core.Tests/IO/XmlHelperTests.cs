24f7dc9e8ff36eba91dadee6ce2e8cc08b37c2cd
Robert Oze <support@4dillusions.com>
2025-10-16 03:55:45 +0200

    add project structure &XmlHelper

---
+++ Project/FW4di.Dotnet.Core.Tests/IO/XmlHelperTests.cs
@@ -0,0 +1,69 @@
+﻿using FW4di.Dotnet.Core.IO;
+
+namespace FW4di.Dotnet.Core.Tests.IO;
+
+[TestClass]
+public class XmlHelperTests
+{
+    public class TestClass
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    private readonly TestClass testObject = new TestClass { Name = "John", Age = 30 };
+    private readonly string expectedXml = "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<TestClass>\r\n  <Name>John</Name>\r\n  <Age>30</Age>\r\n</TestClass>";
+
+    [TestMethod]
+    public void Serialize_ShouldReturnValidXml()
+    {
+        string xml = XmlHelper.Serialize(testObject);
+        Assert.IsTrue(xml.Contains("</TestClass>"));
+        Assert.IsTrue(xml.Contains("<Name>John</Name>"));
+        Assert.IsTrue(xml.Contains("<Age>30</Age>"));
+    }
+
+    [TestMethod]
+    public void Deserialize_ShouldReturnValidObject()
+    {
+        var obj = XmlHelper.Deserialize<TestClass>(expectedXml);
+        Assert.IsNotNull(obj);
+        Assert.AreEqual("John", obj.Name);
+        Assert.AreEqual(30, obj.Age);
+    }
+
+    [TestMethod]
+    public void SerializeToFile_And_DeserializeFromFile_ShouldWork()
+    {
+        string filePath = "test.xml";
+
+        try
+        {
+            XmlHelper.SerializeToFile(testObject, filePath);
+            var deserializedObject = XmlHelper.DeserializeFromFile<TestClass>(filePath);
+
+            Assert.IsNotNull(deserializedObject);
+            Assert.AreEqual("John", deserializedObject.Name);
+            Assert.AreEqual(30, deserializedObject.Age);
+        }
+        finally
+        {
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+        }
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Serialize_NullObject_ShouldThrowException()
+    {
+        XmlHelper.Serialize<TestClass>(null);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Deserialize_NullString_ShouldThrowException()
+    {
+        XmlHelper.Deserialize<TestClass>(null);
+    }
+}
---
+++ Project/FW4di.Dotnet.Core/IO/XmlHelper.cs
@@ -0,0 +1,70 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+namespace FW4di.Dotnet.Core.IO;
+
+public static class XmlHelper
+{
+    public static string Serialize<T>(T? obj)
+    {
+        if (obj == null)
+            throw new ArgumentNullException(nameof(obj));
+
+        var serializer = new XmlSerializer(typeof(T));
+        using (var stringWriter = new StringWriter())
+        {
+            using (var xmlWriter = XmlWriter.Create(stringWriter, new XmlWriterSettings { Indent = true }))
+            {
+                serializer.Serialize(xmlWriter, obj);
+                return stringWriter.ToString();
+            }
+        }
+    }
+
+    public static T Deserialize<T>(string? xml)
+    {
+        if (string.IsNullOrWhiteSpace(xml))
+            throw new ArgumentNullException(nameof(xml));
+
+        var serializer = new XmlSerializer(typeof(T));
+        using (var stringReader = new StringReader(xml))
+        {
+            var result = serializer.Deserialize(stringReader);
+            if (result is null)
+                throw new InvalidOperationException("Deserialization returned null.");
+
+            return (T)result;
+        }
+    }
+
+    public static void SerializeToFile<T>(T? obj, string? filePath)
+    {
+        if (obj == null)
+            throw new ArgumentNullException(nameof(obj));
+        if (string.IsNullOrWhiteSpace(filePath))
+            throw new ArgumentNullException(nameof(filePath));
+
+        var serializer = new XmlSerializer(typeof(T));
+        using (var writer = new StreamWriter(filePath))
+        {
+            serializer.Serialize(writer, obj);
+        }
+    }
+
+    public static T DeserializeFromFile<T>(string? filePath)
+    {
+        if (string.IsNullOrWhiteSpace(filePath))
+            throw new ArgumentNullException(nameof(filePath));
+
+        var serializer = new XmlSerializer(typeof(T));
+        using (var reader = new StreamReader(filePath))
+        {
+            var result = serializer.Deserialize(reader);
+            if (result is null)
+                throw new InvalidOperationException("Deserialization from file returned null.");
+
+            return (T)result;
+        }
+    }
+}
+
