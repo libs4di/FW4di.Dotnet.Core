e6acf5f79acf4af470eca57b0831c4b49a5da925
Robert Oze <support@4dillusions.com>
2025-10-29 23:36:08 +0100

    impl. Mocking

--- Project/FW4di.Dotnet.Core/IO/XmlHelper.cs
+++ Project/FW4di.Dotnet.Core/IO/XmlHelper.cs
@@ -74,4 +74,3 @@ public static class XmlHelper
         }
     }
 }
-
---
+++ Project/FW4di.Dotnet.Core/Mocking/MockHelper.cs
@@ -0,0 +1,27 @@
+﻿/*
+4di Framework .NET Core Library
+Copyright (c) 2025 by 4D Illusions. All rights reserved.
+Released under the terms of the GNU General Public License version 3 or later.
+*/
+
+using Moq;
+
+namespace FW4di.Dotnet.Core.Mocking;
+
+public static class MockHelper
+{
+    public static T Generate<T>() where T : class
+    {
+        return new Mock<T>().Object;
+    }
+
+    public static T Generate<T>(MockBehavior behavior) where T : class
+    {
+        var mockRepo = new MockRepository(behavior)
+        {
+            DefaultValue = DefaultValue.Mock
+        };
+
+        return mockRepo.Create<T>().Object;
+    }
+}
