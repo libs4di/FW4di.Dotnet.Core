90b0671c19858dea166810bf6c1182536cb097bf
Robert Oze <support@4dillusions.com>
2025-10-29 23:24:45 +0100

    Update XmlHelper.cs

--- Project/FW4di.Dotnet.Core/IO/XmlHelper.cs
+++ Project/FW4di.Dotnet.Core/IO/XmlHelper.cs
@@ -1,4 +1,10 @@
-﻿using System.Xml;
+﻿/*
+4di Framework .NET Core Library
+Copyright (c) 2025 by 4D Illusions. All rights reserved.
+Released under the terms of the GNU General Public License version 3 or later.
+*/
+
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace FW4di.Dotnet.Core.IO;
